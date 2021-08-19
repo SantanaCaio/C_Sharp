6663fd617fefd26fd79798b72f34a2379d62eaf4
Caio Bezerra <76970223+SantanaCaio@users.noreply.github.com>
2021-08-19 13:45:31 -0300

    Add files via upload

---
+++ 2# Projeto C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2__Projeto_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
