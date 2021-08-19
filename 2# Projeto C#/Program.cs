34a92fdc43bb21c3fb2fbd9141f4ceee2bcb2497
Caio Bezerra <76970223+SantanaCaio@users.noreply.github.com>
2021-08-19 14:43:10 -0300

    Add files via upload

--- 2# Projeto C#/Program.cs
+++ 2# Projeto C#/Program.cs
@@ -6,7 +6,19 @@ namespace _2__Projeto_C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Soma de dois númetros inteiros");
+
+            Console.WriteLine("insira o primeiro numero");
+            int numeroUm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira o primeiro numero");
+            int numeroDois = int.Parse(Console.ReadLine());
+
+            int soma = numeroDois + numeroUm;
+
+            Console.WriteLine("A soma dos dois valores é " + soma);
+
+
         }
     }
 }
