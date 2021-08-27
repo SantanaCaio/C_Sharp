7a13d9ba0ac1e88caa502725959979e852593f64
Caio Bezerra <76970223+SantanaCaio@users.noreply.github.com>
2021-08-27 17:26:40 -0300

    Add files via upload

--- 14# Projetos C#/Program.cs
+++ 14# Projetos C#/Program.cs
@@ -8,7 +8,10 @@ namespace _14__Projetos_C_
         {
             string respostasP, respostasS;
             bool sexoValido = false;
-            int respostaM = 0, respostaF = 0;
+            int respostaMS = 0, respostaFS = 0;
+            int respostaMN = 0, respostaFN = 0;
+            int cont = 0;
+
             do
             {
                 Console.WriteLine($"Gostou do produto \n ' 1 ' SIM \n ' 2 ' NÂO");
@@ -20,17 +23,28 @@ namespace _14__Projetos_C_
                     Console.WriteLine($"Qual o seu sexo \n ' 1 ' MASCULINO \n ' 2 ' FEMININO");
                     respostasS = Console.ReadLine();
 
-                    if (respostasS == "1")
+                    if (respostasS == "1")// define os sexos das pessoas
                     {
-                        if (respostasP == ' 1 ')
+                        if (respostasP == "1")//divide as respostas das pessoas
                         {
-                            
+                            respostaMS++;
                         }
-                        respostaM++;
+                        else if (respostasP == "2")
+                        {
+                            respostaMN++;
+                        }
+
                     }
                     else if (respostasS == "2")
                     {
-                        respostaF++;
+                        if (respostasP == "1")//divide as respostas das pessoas
+                        {
+                            respostaFS++;
+                        }
+                        else if (respostasP == "2")
+                        {
+                            respostaFN++;
+                        }
                     }
                     else
                     {
@@ -39,19 +53,18 @@ namespace _14__Projetos_C_
                     }
 
                 } while (sexoValido);
+                cont++;
+            } while (cont < 10);
 
+            Console.WriteLine($"O número de pessoas que responderam SIM foram {respostaFS + respostaMS} pessoas");
+            Console.WriteLine($"O número de pessoas que responderam NÂO foram {respostaFN + respostaMN} pessoas");
 
+            Console.WriteLine($"O número de mulheres que responderam SIM foram {respostaFS} mulheres");
 
-
-
-
-
-            } while (true);
+            double percentual = ((respostaMS + respostaMN) * (respostaMN * 100));
+            Console.WriteLine($"O número de homens que responderam NÂO entre os homens analisados foi de {percentual}%");
         }
     }
 }
-// pegar o sexo e se respondeu sim ou não de 10 pessoas
 
-// quantas pessoas responderam sim
-// quantas pessoas responderam não
 
