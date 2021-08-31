f835ddf5379d1ee97f05b11f33eb8e1e23ad31b0
DEV <dev@info.local>
2021-08-31 17:21:06 -0300

    final da aula

---
+++ 16 # projetos C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _16___projetos_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int soma = 0;
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Insira o {i + 1}º de 10 valores");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            for (var i = 0; i < 10; i++)
+            {
+                soma += numeros[i]; 
+            }
+                Console.WriteLine($"a soma dos valores é: {soma}");
+
+        }
+    }
+}
