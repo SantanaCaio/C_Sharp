50e0bea569fd1d568baf05ae48a839961a983ab2
DEV <dev@info.local>
2021-09-01 13:41:44 -0300

    Adionando exercicio 18, de 01/09/2021

---
+++ 18 # Projetos C#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _18___Projetos_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperaturas = new double[12];
+            double temMax = -999, temMin = 999;
+
+            for (var i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"Coloque a temperatura média do mês {i+1}:");
+                temperaturas[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (var i = 0; i < 12; i++)
+            {
+                if (temperaturas[i] > temMax)
+                {
+                    temMax = temperaturas[i];
+                }
+                if (temperaturas[i] < temMin)
+                {
+                    temMin = temperaturas[i];
+                }
+            }
+
+            Console.WriteLine($"A menor temperatura média do ano é {temMin}º, e a maior é {temMax}º");
+            
+        }
+    }
+}
+
+
+//  Calcule e exiba a maior e a menor temperatura do ano.
